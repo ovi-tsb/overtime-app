ffb3fb7703cf7eab1aec3a52d296fafea248398b
Ovidiu Turcu <ovi_tsb@yahoo.com>
2018-06-22 01:02:01 -0400

    Added new post button to nav bar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -26,7 +26,14 @@ describe 'navigate' do
       expect(page).to have_content(/Rationale|content/)
     end
   end
+  describe 'new' do
+    it 'has alink from the homepage' do
+      visit root_path
 
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
   describe 'creation' do
     before do
       visit new_post_path
