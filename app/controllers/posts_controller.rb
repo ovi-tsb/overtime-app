2517d4d40e3cd4aedf6dfe40729b74125b84ef06
Ovidiu Turcu <ovi_tsb@yahoo.com>
2018-07-27 01:13:54 -0400

    Build in approval and review buttons for admin review homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,20 @@
  class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :approve]
 
   def index
     @posts = Post.posts_by(current_user).page(params[:page]).per(10)
   end
+
+  def approve
+    authorize @post
+    @post.approved!
+    redirect_to root_path, notice: "The post has been approved"
+  end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
