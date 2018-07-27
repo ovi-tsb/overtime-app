b5a97589fc78a0150e6cb2927f2c9af4a4999c03
Ovidiu Turcu <ovi_tsb@yahoo.com>
2018-07-27 00:33:15 -0400

    Brought in live date for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,12 @@ AdminUser.create(email: "admin@yahoo.com",
 puts "1 Admin User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+        nulla pariatur.", user_id: @user.id, overtime_request: 2.5)
 end
 puts "100 Posts have been created"
 
