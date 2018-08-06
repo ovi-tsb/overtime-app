001386ba1cb7739e5ef8843164e94aa3ace28e43
Ovidiu Turcu <ovi_tsb@yahoo.com>
2018-08-06 02:45:33 -0400

    Integrated basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,8 +20,6 @@ Rails.application.configure do
 
   config.active_storage.service = :local
 
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   config.active_support.deprecation = :log
@@ -36,5 +34,7 @@ Rails.application.configure do
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
