aa671d7ffb5e1ea7df8bcc891d299d612aa01002
Ovidiu Turcu <ovi_tsb@yahoo.com>
2018-08-10 01:35:33 -0400

    Built out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-@user = User.create(email: "test@test.com",
+@employee = Employee.create(email: "test@test.com",
                     password: "asdfasdf", 
                     password_confirmation: "asdfasdf", 
                     first_name: "Jon", 
                     last_name: "Snow",
-                    phone: "7052303440")
+                    phone: "4389327925")
 
-puts "1 User created"
+puts "1 Employee created"
 
 AdminUser.create(email: "admin@yahoo.com", 
                  password: "123456", 
@@ -16,9 +16,9 @@ AdminUser.create(email: "admin@yahoo.com",
 
 puts "1 Admin User created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 Audit logs have been created"
 
@@ -28,7 +28,7 @@ puts "3 Audit logs have been created"
         dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
         ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
         dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-        nulla pariatur.", user_id: @user.id, overtime_request: 2.5)
+        nulla pariatur.", user_id: @employee.id, overtime_request: 2.5)
 end
 puts "100 Posts have been created"
 
